abe175ae12042bf64ce572dda83457ee99cb2713
Daniel Krippner <dk.mailbox@gmx.net>
2022-09-01 22:34:28 +0200

    Add pre-defined arm positions

--- src/main.py
+++ src/main.py
@@ -1,35 +1,56 @@
 #!/usr/bin/python3
 import servocontrol as sc
-import servodefs
+import servodefs as sd
 import logging
 
+turn = None
+shoulder = None
+elbow = None
+hand = None
 
 def main():
-    turn = sc.servocontrol( **servodefs.turn )
-    shoulder = sc.servocontrol( **servodefs.shoulder )
-    elbow = sc.servocontrol( **servodefs.elbow )
-    hand = sc.servocontrol( **servodefs.hand )
+    global turn
+    global shoulder
+    global elbow
+    global hand
+
+    turn = sc.servocontrol( **sd.turn )
+    shoulder = sc.servocontrol( **sd.shoulder )
+    elbow = sc.servocontrol( **sd.elbow )
+    hand = sc.servocontrol( **sd.hand )
 
     # do stuff here
-    logging.info( "current turn pos: " + str(turn.get_position_raw()) )
-    turn.drive_to_raw( 1234 )
-    logging.info( "new turn pos: " + str(turn.get_position_raw()) )
+    turn.drive_to( 44 )
+    report_pos()
 
-    turn.drive_to( 0 )
-    logging.info( "new turn pos: " + str(turn.get_position()) )
-
-    turn.drive_to( 50 )
-    logging.info( "new turn pos: " + str(turn.get_position()) )
-
-    turn.drive_to( 100 )
-    logging.info( "new turn pos: " + str(turn.get_position()) )
+    drive_to_pos( **sd.pos_home )
+    report_pos()
 
     del turn
     del shoulder
     del elbow
     del hand
 
+def drive_to_pos( turn_target, shoulder_target, elbow_target, hand_target ):
+    global turn
+    global shoulder
+    global elbow
+    global hand
+    turn.drive_to( turn_target )
+    shoulder.drive_to( shoulder_target )
+    elbow.drive_to( elbow_target )
+    hand.drive_to( hand_target )
 
+def report_pos():
+    global turn
+    global shoulder
+    global elbow
+    global hand
+    logging.info("turn:" + str(turn.get_position()) +
+                " - shoulder:" + str(shoulder.get_position()) +
+                " - elbow:" + str(elbow.get_position()) +
+                " - hand:" + str(hand.get_position()) )
+    
 if __name__ == '__main__':
     import logging.config
     logging.basicConfig( level = logging.INFO )
--- src/servodefs.py
+++ src/servodefs.py
@@ -5,3 +5,7 @@ turn = { 'name': 'turn', 'pin': 17, 'min': 500, 'max': 1500, 'initial': 1000 }
 shoulder = { 'name': 'shoulder', 'pin': 18, 'min': 800, 'max': 1750, 'initial': 1000 }
 elbow = { 'name': 'elbow', 'pin': 19, 'min': 500, 'max': 1500, 'initial': 800 }
 hand = { 'name': 'hand', 'pin': 20, 'min': 625, 'max': 1500, 'initial': 625 }
+
+
+# Set of pre-defined standard positions
+pos_home = { 'turn_target': 50, 'shoulder_target': 50, 'elbow_target': 50, 'hand_target': 0 }
